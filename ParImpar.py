859ce0f677f7b049b1520f396e47a6106658f2de
CamiloPuche <104383069+CamiloPuche@users.noreply.github.com>
2022-04-26 07:17:46 -0500

    Add files via upload

---
+++ ParImpar.py
@@ -0,0 +1,7 @@
+print()
+numero = int(input("Ingresa un numero por favor: "))
+
+if numero % 2 == 0:
+    print(f"El valor {numero} es un numero par ")
+else:
+    print(f"El valor {numero} es un numero impar ")
